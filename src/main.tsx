3dec8750243bfa9091ef2fe8ce94479a1af3488b
Cristian Antonio Tapia <Cristian.Antonio.Tapia@sqm.com>
2026-05-08 04:02:22 -0400

    debug: red background in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+document.body.style.backgroundColor = 'red';
 createRoot(document.getElementById("root")!).render(<App />);
